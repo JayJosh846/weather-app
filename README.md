04f3eaf80a9fc7a53d7663401afc1aaa4433e73e
Jeminusi Jesudara <jesudara.j@gmail.com>
2020-06-29 17:56:39 +0100

    commited files

---
+++ README.md
@@ -0,0 +1,3 @@
+An application that shows the weather of any location of choice that a user enters. 
+
+Weather data is gotten by making GET requests to the servers of Open Weather Map
---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res)
+{
+     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res)
+{
+
+    const query = req.body.cityName;
+    const apiKey = "bfa836c50aca56fc0b2b2685c768306f";
+    const unit = "metric";
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" +query+ "&appid=" +apiKey +"&units=" +unit;
+    https.get(apiURL, function(response) 
+    {
+        console.log(response.statusCode);
+        
+        response.on("data", function(data)
+        {
+            const apiData = JSON.parse(data);
+            const temperature = apiData.main.temp;
+            const weatherDescrip = apiData.weather[0].description;
+            const weatherIcon = apiData.weather[0].icon;
+            const weatherIconURL = "http://openweathermap.org/img/wn/" +weatherIcon +"@2x.png";
+            res.write("<h1>The temperature in " +query + " is " +temperature + " degress celcious.</h1>");
+            res.write("<p>The weather condition is " +weatherDescrip +".</p>");
+            res.write("<img src= " +weatherIconURL +">");
+            res.send();
+        })
+    });
+})
+
+app.listen(3000, function()
+{
+    console.log("Server running on port 3000");    
+});
